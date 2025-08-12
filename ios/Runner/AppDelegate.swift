74b707090bfda3bf059b33e4c7f6e080625e5ece
Dmytro Davydov <itworksinua@gmail.com>
2025-08-12 22:42:24 +0300

    Added two methods for sending oush to update token to server

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -22,10 +22,19 @@ import UIKit
               return
           }
           
-#warning("Check this")
           if call.method == "getLiveActivityPushToken" {
               let token = LiveActivityTokenStorage.shared.getTokensForServer()
               result(token)
+              return
+          }
+          
+          if call.method == "sendRequestDetails" {
+              if let arg = call.arguments as? [String : Any],
+                 let authorization = arg["authorization"] as? String,
+                 let url = arg["url"] as? String{
+                  self.saveRequestDetails(authorization, url: url)
+              }
+              result(nil)
               return
           }
           
@@ -65,5 +74,42 @@ import UIKit
     
     private func endAllLiveActivity() {
         liveActivityService.endAll()
+    }
+    
+    func saveRequestDetails(_ authorization: String, url: String) {
+        // save these two strings for later usage
+    }
+    
+    // Call this method where the update token is received
+    private func sendUpdateTokenToServer(_ token: String, url: String, authorization: String) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(authorization)", forHTTPHeaderField: "Authorization")
+        //refreshActivityToken
+        let postData: [String: Any] = ["refreshActivityToken": "token"] // Replace with your data
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: postData, options: [])
+        } catch {
+            print("Error creating JSON data: \(error)")
+            return
+        }
+        Task {
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request)
+                
+                guard let httpResponse = response as? HTTPURLResponse,
+                      (200...299).contains(httpResponse.statusCode) else {
+                    print("Invalid response or status code.")
+                    return
+                }
+                
+                // Process the response data
+                print("Response data: \(String(data: data, encoding: .utf8) ?? "N/A")")
+                
+            } catch {
+                print("Error during API call: \(error.localizedDescription)")
+            }
+        }
     }
 }
